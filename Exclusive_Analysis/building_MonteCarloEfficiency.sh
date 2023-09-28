170b7f409f0dc6215aed240530fce9a2995872fa
Leszek Adamczyk <leszek.adamczyk@agh.edu.pl>
2023-09-28 17:54:53 +0200

    add building_MonteCarloEfficiency.sh

---
+++ Exclusive_Analysis/building_MonteCarloEfficiency.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+rm -r build/MonteCarloEfficiency
+cd build
+
+export LD_LIBRARY_PATH=../../star-upc/build/:$ROOTSYS/lib
+g++ ../src/ReadPicoLambdaK0.cxx  ../MonteCarloEfficiency.cxx  -o MonteCarloEfficiency -Wl,--copy-dt-needed-entries `root-config --cflags` `root-config --libs` -I ../../star-upc/include/ -I ../include -L ../../star-upc/build/ -lstar-upc
