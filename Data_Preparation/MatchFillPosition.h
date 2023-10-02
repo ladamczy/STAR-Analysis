20a70210a03b605a1bd04f2edd5ad897646362c5
Patrycja <pmalinow@student.agh.edu.pl>
2023-10-02 14:13:29 +0200

    included slope of the beam in reader and fill-beam position matching scripts

--- Data_Preparation/MatchFillPosition.h
+++ Data_Preparation/MatchFillPosition.h
@@ -3,7 +3,6 @@
 
 #include <vector> 
 using namespace std;
-vector <double> FindPosition(int nFillNumber, vector <double> &vFillNumber,  vector <double> &vXPosition, vector <double> &vYPosition,   vector <double> &vX2Position, vector <double> &vY2Position);
-
+vector <double> FindPosition(int nFillNumber, double zPos, vector <double> &vFillNumber,  vector <double> &vXPosition, vector <double> &vYPosition,  vector <double> &vX2Position, vector <double> &vY2Position, vector <double> &vXSlope, vector <double> &vYSlope,   vector <double> &vX2Slope, vector <double> &vY2Slope);
 #endif
 
