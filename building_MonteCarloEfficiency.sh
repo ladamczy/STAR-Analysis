a0e4a8b4ebd514a9d84e7468beeb4876fab54fdb
Krzemon <przemek12413@gmail.com>
2023-10-15 14:58:42 +0200

    test2-cp modified

--- building_MonteCarloEfficiency.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bash
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-rm -r build/MonteCarloEfficiency
-cd build
-
-g++ $DIR/MonteCarloEfficiency.cxx  -o MonteCarloEfficiency -Wl,--copy-dt-needed-entries `root-config --cflags` `root-config --libs` -I $DIR/../star-upc/include/ -I $DIR/include -L $DIR/../star-upc/build/ -lstar-upc
