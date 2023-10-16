e9bd9d3a4270ae7fd35eea29463d83f56948cf5d
Krzemon <przemek12413@gmail.com>
2023-10-16 22:39:36 +0200

    test2 dziala

--- test2/include/ReadPicoLambdaK0.h
+++ test2/include/ReadPicoLambdaK0.h
@@ -32,7 +32,7 @@ public:
 
 private:
     Int_t eventId, p1_hasTOFinfo, p2_hasTOFinfo, pair_charge, p1_ch;
-    Double_t lead_pt, lead_phi, lead_eta,
+    Float_t lead_pt, lead_phi, lead_eta,
              sublead_pt, sublead_phi, sublead_eta,
              p1_pt, p1_phi, p1_eta,
              p2_pt, p2_phi, p2_eta,
