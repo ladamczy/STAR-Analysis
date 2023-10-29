701a897a0de4e0c2ca50540b3c2ce1188473ff5a
Adam W <adam.jan@vp.pl>
2023-10-29 20:06:51 +0100

    Minor fixes for Pythia simulation

--- Inclusive_Analysis/pythia/include/XiHistogramsClass.h
+++ Inclusive_Analysis/pythia/include/XiHistogramsClass.h
@@ -27,9 +27,9 @@ XiHistogramsClass::XiHistogramsClass(string namepart, string identifier=""){
         identifier = to_string(objectCount);
     }
     objectCount++;
-    xi_e_w = new TH2D(string("xi_e_w_"+identifier).c_str(), string("#Xi_{E} vs #Xi_{W}" + namepart + ";Xi_{E};Xi_{W}").c_str(), 50, 0, 1, 50, 0, 1);
-    xi_multiplied = new TH1D(string("xi_multiplied_"+identifier).c_str(), string("#Xi_{E}*#Xi_{W}" + namepart + ";Xi_{E}*Xi_{W};entries").c_str(), 60, 0, 0.3);
-    xi_log = new TH1D(string("xi_log_"+identifier).c_str(), string("ln(#frac{#Xi_{E}}{#Xi_{W}})" + namepart + ";ln(#frac{#Xi_{E}}{#Xi_{W}});entries").c_str(), 100, -10, 10);
+    xi_e_w = new TH2D(string("xi_e_w_"+identifier).c_str(), string("#xi_{E} vs #xi_{W}"+namepart+";#xi_{E};#xi_{W}").c_str(), 50, 0, 1, 50, 0, 1);
+    xi_multiplied = new TH1D(string("xi_multiplied_"+identifier).c_str(), string("#xi_{E}*#xi_{W}"+namepart+";#xi_{E}*#xi_{W};entries").c_str(), 60, 0, 0.3);
+    xi_log = new TH1D(string("xi_log_"+identifier).c_str(), string("ln(#xi_{E}/#xi_{W})"+namepart+";ln(#xi_{E}/#xi_{W});entries").c_str(), 100, -10, 10);
 }
 
 XiHistogramsClass::~XiHistogramsClass(){
