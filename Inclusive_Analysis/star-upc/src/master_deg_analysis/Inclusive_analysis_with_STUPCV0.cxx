19c60be3baf97ddd459f8349b7fb3e32addd985f
Adam W <adam.jan@vp.pl>
2023-12-05 18:57:23 +0100

    Added histograms

--- Inclusive_Analysis/star-upc/src/master_deg_analysis/Inclusive_analysis_with_STUPCV0.cxx
+++ Inclusive_Analysis/star-upc/src/master_deg_analysis/Inclusive_analysis_with_STUPCV0.cxx
@@ -51,7 +51,15 @@ int main(int argc, char **argv){
 
     //histograms
     ProcessingOutsideLoop outsideprocessing;
-    outsideprocessing.AddHistogram(TH1D("MpipiforMatchHist", "K^{0}_{S} mass;m_{#pi^{+}#pi^{-}} [GeV];Number of pairs", 100, kaonMassWindowWideLow, kaonMassWindowWideHigh));
+    outsideprocessing.AddHistogram(TH1D("Mpipibefore", "K^{0}_{S} mass;m_{#pi^{+}#pi^{-}} [GeV];Number of pairs", 100, kaonMassWindowWideLow, kaonMassWindowWideHigh));
+    outsideprocessing.AddHistogram(TH1D("DCApipiK0", "DCA between #pi^{#pm} from leading K^{0}_{S};DCA_{#pi^{+}#pi^{-}-K^{0}_{S}};Number of pairs", 50, 0, 5));
+    outsideprocessing.AddHistogram(TH1D("DCApipiPV", "DCA between #pi^{#pm} from subleading K^{0}_{S} (PV) when in narrow mass window;DCA_{#pi^{+}#pi^{-}-PV};Number of pairs", 50, 0, 5));
+    outsideprocessing.AddHistogram(TH1D("DCAK0PV", "DCA between leading K^{0}_{S} and subleading K^{0}_{S};DCA_{#pi^{+}#pi^{-}-K^{0}_{S}};Number of pairs", 50, 0, 5));
+    outsideprocessing.AddHistogram(TH1D("LogProtons", "log(#xi_{E}*#xi_{W});log(#xi_{E}*#xi_{W});events", 200, -20, 0));
+    outsideprocessing.AddHistogram(TH1D("DivProtons", "ln(#xi_{E}/#xi_{W});ln(#xi_{E}/#xi_{W});events", 100, -10, 10));
+    outsideprocessing.AddHistogram(TH2D("Log2DProtons", "log#xi_{W} vs log#xi_{E};log#xi_{E};log#xi_{W}", 100, -9, 1, 100, -9, 1));
+    outsideprocessing.AddHistogram(TH1D("Mpipiafter", "K^{0}_{S} mass;m_{#pi^{+}#pi^{-}} [GeV];Number of pairs", 100, kaonMassWindowWideLow, kaonMassWindowWideHigh));
+
     // int triggers[] = { 570701, 570705, 570711, 590701, 590705, 590708 };
     // outsideprocessing.AddHistogram(TH1D("triggerHist", "Data triggers;Trigger ID;Number of events", 6, 0, 6));
     // for(int i = 0;i<6;i++){
@@ -153,14 +161,42 @@ int main(int argc, char **argv){
                 continue;
             }
             //setting second vertex
+            bool isSubleadingK0Present = false;
             TVector3 correctedVertex;
             if(subleading_pair_indices[0]<0){
                 correctedVertex = { beamValues[0], beamValues[1], tempUPCpointer->getVertex(0)->getPosZ() };
             } else{
                 correctedVertex = subleading_particle->decayVertex();
+                isSubleadingK0Present = true;
             }
 
+            // TH1D("Mpipibefore", "K^{0}_{S} mass;m_{#pi^{+}#pi^{-}} [GeV];Number of pairs", 100, kaonMassWindowWideLow, kaonMassWindowWideHigh));
+            // TH1D("DCApipiK0", "DCA between #pi^{#pm} from leading K^{0}_{S};DCA_{#pi^{+}#pi^{-}-K^{0}_{S}};Number of pairs", 50, 0, 5));
+            // TH1D("DCApipiPV", "DCA between #pi^{#pm} from subleading K^{0}_{S} (PV) when in narrow mass window;DCA_{#pi^{+}#pi^{-}-PV};Number of pairs", 50, 0, 5));
+            // TH1D("DCAK0PV", "DCA between leading K^{0}_{S} and subleading K^{0}_{S};DCA_{#pi^{+}#pi^{-}-K^{0}_{S}};Number of pairs", 50, 0, 5));
+            // TH1D("LogProtons", "log(#xi_{E}*#xi_{W});log(#xi_{E}*#xi_{W});events", 60, -6, 0));
+            // TH1D("DivProtons", "ln(#xi_{E}/#xi_{W});ln(#xi_{E}/#xi_{W});events", 100, -10, 10));
+            // TH2D("Log2DProtons", "log#xi_{W} vs log#xi_{E};log#xi_{E};log#xi_{W}", 50, -4, 1, 50, -4, 1));
+            // TH1D("Mpipiafter", "K^{0}_{S} mass;m_{#pi^{+}#pi^{-}} [GeV];Number of pairs", 100, kaonMassWindowWideLow, kaonMassWindowWideHigh));
+
             insideprocessing.Fill(0, leading_particle->m());
+            insideprocessing.Fill(1, leading_particle->dcaDaughters());
+            if(isSubleadingK0Present&&subleading_particle->m()>kaonMassWindowNarrowLow&&subleading_particle->m()<kaonMassWindowNarrowHigh){
+                insideprocessing.Fill(2, subleading_particle->dcaDaughters());
+                insideprocessing.Fill(3, (leading_particle->decayVertex()-subleading_particle->decayVertex()).Mag());
+                if(subleading_particle->dcaDaughters()<1.5&&subleading_particle->DCABeamLine()<1.5&&subleading_particle->pointingAngleHypo()>0.925){
+                    insideprocessing.Fill(7, leading_particle->m());
+                }
+            }
+            insideprocessing.Fill(4, log(tempRPpointer->getTrack(0)->xi(255.0)*tempRPpointer->getTrack(1)->xi(255.0)));
+            //0th track is east if branch <2
+            if(tempRPpointer->getTrack(0)->branch()<2){
+                insideprocessing.Fill(5, log(tempRPpointer->getTrack(0)->xi(255.0)/tempRPpointer->getTrack(1)->xi(255.0)));
+                insideprocessing.Fill(6, log(tempRPpointer->getTrack(0)->xi(255.0)), log(tempRPpointer->getTrack(1)->xi(255.0)));
+            } else{
+                insideprocessing.Fill(5, log(tempRPpointer->getTrack(1)->xi(255.0)/tempRPpointer->getTrack(0)->xi(255.0)));
+                insideprocessing.Fill(6, log(tempRPpointer->getTrack(1)->xi(255.0)), log(tempRPpointer->getTrack(0)->xi(255.0)));
+            }
 
             // //used triggers histograms
             // for(int i = 0;i<6;i++){
