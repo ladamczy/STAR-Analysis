beb7ec99afa96db3e9fd00dc7ffa24dbca95c05f
Adam J Wątroba <adamwatroba@rcas6012.rcf.bnl.gov>
2024-04-30 07:45:33 -0400

    Found STAR Pythia scripts

---
+++ Inclusive_Analysis/pythia/STAR_scripts/run_all.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+root4star -q -b elastic.C\($1\)
+root4star -q -b singleDiffractive.C\($1\)
+root4star -q -b doubleDiffractive.C\($1\)
+root4star -q -b centralDiffractive.C\($1\)
+root4star -q -b nonDiffractive.C\($1\)
