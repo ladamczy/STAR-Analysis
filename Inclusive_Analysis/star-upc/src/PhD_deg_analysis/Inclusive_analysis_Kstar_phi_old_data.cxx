fd91073763160db5a4f5a98e782bd88475819939
Adam W <adam.jan@vp.pl>
2024-10-13 13:12:38 +0200

    Kstar first histogram

---
+++ Inclusive_Analysis/star-upc/src/PhD_deg_analysis/Inclusive_analysis_Kstar_phi_old_data.cxx
@@ -0,0 +1,180 @@
+//cpp headers
+
+//ROOT headers
+#include <ROOT/TThreadedObject.hxx>
+#include <TTreeReader.h>
+#include <ROOT/TTreeProcessorMT.hxx>
+
+// picoDst headers
+#include "StRPEvent.h"
+#include "StUPCRpsTrack.h"
+#include "StUPCRpsTrackPoint.h"
+#include "StUPCEvent.h"
+#include "StUPCTrack.h"
+#include "StUPCBemcCluster.h"
+#include "StUPCVertex.h"
+#include "StUPCTofHit.h"
+#include "StUPCV0.h"
+#include "BeamPosition.h"
+#include "StPicoPhysicalHelix.h"
+
+//my headers
+#include "UsefulThings.h"
+#include "ProcessingInsideLoop.h"
+#include "ProcessingOutsideLoop.h"
+
+enum{
+    kAll = 1, kCPT, kRP, kOneVertex, kTPCTOF,
+    kTotQ, kMax
+};
+enum SIDE{ E = 0, East = 0, W = 1, West = 1, nSides };
+enum PARTICLES{ Pion = 0, Kaon = 1, Proton = 2, nParticles };
+const double particleMass[nParticles] = { 0.13957, 0.497611, 0.93827 }; // pion, kaon, proton in GeV /c^2 
+enum BRANCH_ID{ EU, ED, WU, WD, nBranches };
+enum RP_ID{ E1U, E1D, E2U, E2D, W1U, W1D, W2U, W2D, nRomanPots };
+
+int main(int argc, char **argv){
+
+    int nthreads = 1;
+    if(argc==4){
+        nthreads = atoi(argv[3]);
+    }
+
+    cout<<"Program is running on "<<nthreads<<" threads"<<endl;
+    ROOT::EnableThreadSafety();
+    //actually i'm not sure if it's needed here
+    // ROOT::EnableImplicitMT(nthreads); //turn on multicore processing
+
+    //preparing input & output
+    TChain *upcChain = new TChain("mUPCTree");
+    if(ConnectInput(argc, argv, upcChain)){
+        cout<<"All files connected"<<endl;
+    }
+    const string &outputFolder = argv[2];
+
+    //useful constants
+    vector<vector<double>> beamData = ReadFillPositionData("STAR-Analysis/share/Run7PolarizationWithPosition.csv");
+
+    //histograms
+    ProcessingOutsideLoop outsideprocessing;
+    //phi, Kstar
+    outsideprocessing.AddHistogram(TH1D("MKKWide", "#phi mass in wide range;m_{K^{+}K^{-}} [GeV];Number of pairs", 500, 0.9, 5.9));
+    // outsideprocessing.AddHistogram(TH1D("MKpiWide", "K^{*} mass in wide range;m_{K^{#pm}#pi^{#mp}} [GeV];Number of pairs", 500, 0.5, 5.5));
+
+    //processing
+    //defining TreeProcessor
+    ROOT::TTreeProcessorMT TreeProc(*upcChain, nthreads);
+
+    int eventsProcessed = 0;
+
+    //defining processing function
+    auto myFunction = [&](TTreeReader &myReader){
+        //getting values from TChain, in-loop histogram initialization
+        TTreeReaderValue<StUPCEvent> StUPCEventInstance(myReader, "mUPCEvent");
+        // TTreeReaderValue<StRPEvent> StRPEventInstance(myReader, "mRPEvent");
+        ProcessingInsideLoop insideprocessing;
+        StUPCEvent *tempUPCpointer;
+        // StRPEvent *tempRPpointer;
+        insideprocessing.GetLocalHistograms(&outsideprocessing);
+
+        //helpful variables
+        std::vector<StUPCTrack *> vector_Track_positive;
+        std::vector<StUPCTrack *> vector_Track_negative;
+        StUPCTrack *tempTrack;
+        TVector3 vertexPrimary;
+        std::vector<double> tempBeamVector;
+        double beamValues[4];
+        StUPCV0 *tempParticle;
+
+        //actual loop
+        while(myReader.Next()){
+            //in a TTree, it *would* be constant, in TChain however not necessarily
+            tempUPCpointer = StUPCEventInstance.Get();
+            // tempRPpointer = StRPEventInstance.Get();
+
+            //cleaning the loop
+            vector_Track_positive.clear();
+            vector_Track_negative.clear();
+
+            //cause I want to see what's going on
+            if(eventsProcessed%10000==0){
+                cout<<"Processed "<<eventsProcessed<<" events"<<endl;
+            }
+            eventsProcessed++;
+
+            //cuts & histogram filling
+            //selecting tracks matching criteria:
+            //TOF
+            //pt & eta 
+            //Nhits
+            for(int i = 0; i<tempUPCpointer->getNumberOfTracks(); i++){
+                tempTrack = tempUPCpointer->getTrack(i);
+                if(!tempTrack->getFlag(StUPCTrack::kTof)){
+                    continue;
+                }
+                if(tempTrack->getPt()<=0.2 or abs(tempTrack->getEta())>=0.9){
+                    continue;
+                }
+                if(tempTrack->getNhits()<=20){
+                    continue;
+                }
+                if(tempTrack->getCharge()>0){
+                    vector_Track_positive.push_back(tempTrack);
+                } else{
+                    vector_Track_negative.push_back(tempTrack);
+                }
+            }
+            //selecting all track matching criteria:
+            // DCAdaugter<=2.5cm
+            // DCAbeamline<=2.5cm
+            // decayLenghth<3cm or cos(pointing angle)>0.925
+            vertexPrimary = { tempUPCpointer->getVertex(0)->getPosX(), tempUPCpointer->getVertex(0)->getPosY(), tempUPCpointer->getVertex(0)->getPosZ() };
+            tempBeamVector = FindPosition(tempUPCpointer->getFillNumber(), vertexPrimary.Z(), beamData[0], beamData[1], beamData[2], beamData[3], beamData[4], beamData[5], beamData[6], beamData[7], beamData[8]);
+            beamValues[0] = tempBeamVector[0];
+            beamValues[1] = tempBeamVector[1];
+            beamValues[2] = tempBeamVector[2];
+            beamValues[3] = tempBeamVector[3];
+
+            //actual loop for Kstar and phi (phi not yet)
+            for(long unsigned int i = 0; i<vector_Track_positive.size(); i++){
+                for(long unsigned int j = 0; j<vector_Track_negative.size(); j++){
+                    tempParticle = new StUPCV0(vector_Track_positive[i], vector_Track_negative[j], particleMass[1], particleMass[1], 1, 1, vertexPrimary, beamValues, tempUPCpointer->getMagneticField(), false);
+                    //tests if accept the particle
+                    bool TrackTest2 = tempParticle->dcaDaughters()<=2.5;
+                    bool TrackTest3 = tempParticle->DCABeamLine()<=2.5;
+                    bool TrackTest4 = tempParticle->pointingAngleHypo()>0.925;
+                    bool TrackTest5 = tempParticle->decayLengthHypo()<3.0;
+                    //tests before filling
+                    if(!(TrackTest2&&TrackTest3&&(TrackTest4||TrackTest5))){
+                        delete tempParticle;
+                        continue;
+                    }
+                    //filling
+                    insideprocessing.Fill("MKKWide", tempParticle->m());
+                    //finishing
+                    delete tempParticle;
+                }
+            }
+        }
+        return 0;
+        };
+
+    TreeProc.Process(myFunction);
+
+    outsideprocessing.Merge();
+
+    //setting up a tree & output file
+    string path = string(argv[0]);
+    string outfileName;
+    if(outputFolder.find(".root")!=std::string::npos){
+        outfileName = outputFolder;
+    } else{
+        outfileName = outputFolder+"AnaOutput_"+path.substr(path.find_last_of("/\\")+1)+".root";
+    }
+    cout<<"Created output file "<<outfileName<<endl;
+    TFile *outputFileHist = TFile::Open(outfileName.c_str(), "recreate");
+    outsideprocessing.SaveToFile(outputFileHist);
+    outputFileHist->Close();
+
+    return 0;
+}
