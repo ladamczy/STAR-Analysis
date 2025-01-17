2c7e28dd7f875f14ca589d3298dc02224d65f8b6
bsnehar <sbhosale@nat110-dyn-045.fis.agh.edu.pl>
2025-01-17 23:24:50 +0100

    added .gch files to be ignored

--- Exclusive_Analysis/building.sh
+++ Exclusive_Analysis/building.sh
@@ -10,4 +10,5 @@ rm -r build
 mkdir build
 cd build
 export LD_LIBRARY_PATH=../../star-upc/build/:$ROOTSYS/lib
-g++ ../src/MatchFillPosition.cxx ../include/* ../src/ReadFillPositionFile.cxx ../$1.cxx -o $1 -Wl,--copy-dt-needed-entries `root-config --cflags` `root-config --libs` -I ../../star-upc/include/ -I ../include -L ../../star-upc/build/ -lstar-upc
+g++ /home/sbhosale/Work/STAR-Analysis/Exclusive_Analysis/src/MatchFillPosition.cxx /home/sbhosale/Work/STAR-Analysis/Exclusive_Analysis/src/ReadFillPositionFile.cxx ../ExclusiveAnalysisStUPCV0.cxx -o ExclusiveAnalysisStUPCV0 -Wl,--copy-dt-needed-entries `root-config --cflags` `root-config --libs` -I /home/sbhosale/Work/STAR-Analysis/star-upc/include/ -I /home/sbhosale/Work/STAR-Analysis/Exclusive_Analysis/include -L /home/sbhosale/Work/STAR-Analysis/star-upc/build/ -lstar-upc
+#g++ ../src/MatchFillPosition.cxx ../include/* ../src/ReadFillPositionFile.cxx ../$1.cxx -o $1 -Wl,--copy-dt-needed-entries `root-config --cflags` `root-config --libs` -I ../../star-upc/include/ -I ../include -L ../../star-upc/build/ -lstar-upc
