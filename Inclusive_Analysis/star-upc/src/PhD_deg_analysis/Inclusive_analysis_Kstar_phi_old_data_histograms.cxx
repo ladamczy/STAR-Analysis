0c65dec52d1955819811edf50b4e2fe49395e361
Adam W <adam.jan@vp.pl>
2025-03-19 09:52:04 +0100

    Added drawing sgnal minus background plots

--- Inclusive_Analysis/star-upc/src/PhD_deg_analysis/Inclusive_analysis_Kstar_phi_old_data_histograms.cxx
+++ Inclusive_Analysis/star-upc/src/PhD_deg_analysis/Inclusive_analysis_Kstar_phi_old_data_histograms.cxx
@@ -11,7 +11,10 @@
 #include "TCanvas.h"
 #include "TStyle.h"
 
-void draw_and_save(TH1D* data, std::string folderWithDiagonal, std::string name);
+#include "MyStyles.h"
+
+void draw_and_save(TH1D* data, std::string folderWithDiagonal, std::string name, std::string title);
+void draw_and_save_minus_background(TH1D* data, TH1D* bcg, std::string folderWithDiagonal, std::string name, std::string title, double bcg_region);
 
 int main(int argc, char const* argv[]){
     TFile* input = TFile::Open(argv[1]);
@@ -35,7 +38,20 @@ int main(int argc, char const* argv[]){
     MppChi2Wide
     */
 
-    std::string folderWithDiagonal = std::string(argv[2]);
+    TFile* inputbcg = TFile::Open(argv[2]);
+
+    //getting background histograms out
+    //MIXEDTOF, NORMALTOF, NOTOF
+    std::string background = "NORMALTOF";
+    TH1D* MKpiChi2bcg = (TH1D*)inputbcg->Get(("MKpiChi2bcg"+background).c_str());
+    TH1D* MpiKChi2bcg = (TH1D*)inputbcg->Get(("MpiKChi2bcg"+background).c_str());
+    TH1D* MppiChi2bcg = (TH1D*)inputbcg->Get(("MppiChi2bcg"+background).c_str());
+    TH1D* MpipChi2bcg = (TH1D*)inputbcg->Get(("MpipChi2bcg"+background).c_str());
+    TH1D* MKKChi2bcg = (TH1D*)inputbcg->Get(("MKKChi2bcg"+background).c_str());
+    TH1D* MpipiChi2bcg = (TH1D*)inputbcg->Get(("MpipiChi2bcg"+background).c_str());
+    TH1D* MppChi2bcg = (TH1D*)inputbcg->Get(("MppChi2bcg"+background).c_str());
+
+    std::string folderWithDiagonal = std::string(argv[3]);
     if(folderWithDiagonal[folderWithDiagonal.size()-1]!='/'){
         folderWithDiagonal += "/";
     }
@@ -61,24 +77,39 @@ int main(int argc, char const* argv[]){
     resultCanvas->SaveAs((folderWithDiagonal+"pairInfo.pdf").c_str());
 
     //the rest
-    draw_and_save(MKpiChi2Wide, folderWithDiagonal, "MKpi");
-    draw_and_save(MpiKChi2Wide, folderWithDiagonal, "MpiK");
-    draw_and_save(MppiChi2Wide, folderWithDiagonal, "Mppi");
-    draw_and_save(MpipChi2Wide, folderWithDiagonal, "Mpip");
-    draw_and_save(MKKChi2Wide, folderWithDiagonal, "MKK");
-    draw_and_save(MpipiChi2Wide, folderWithDiagonal, "Mpipi");
-    draw_and_save(MppChi2Wide, folderWithDiagonal, "Mpp");
+    draw_and_save(MKpiChi2Wide, folderWithDiagonal, "MKpi", "K^{+}#pi^{-}");
+    draw_and_save(MpiKChi2Wide, folderWithDiagonal, "MpiK", "#pi^{+}K^{-}");
+    draw_and_save(MppiChi2Wide, folderWithDiagonal, "Mppi", "p^{+}#pi^{-}");
+    draw_and_save(MpipChi2Wide, folderWithDiagonal, "Mpip", "#pi^{+}p^{-}");
+    draw_and_save(MKKChi2Wide, folderWithDiagonal, "MKK", "K^{+}K^{-}");
+    draw_and_save(MpipiChi2Wide, folderWithDiagonal, "Mpipi", "#pi^{+}#pi^{-}");
+    draw_and_save(MppChi2Wide, folderWithDiagonal, "Mpp", "p^{+}p^{-}");
+    draw_and_save_minus_background(MKpiChi2Wide, MKpiChi2bcg, folderWithDiagonal, "MKpiFit", "K^{+}#pi^{-}", 1.0);
+    draw_and_save_minus_background(MpiKChi2Wide, MpiKChi2bcg, folderWithDiagonal, "MpiKFit", "#pi^{+}K^{-}", 1.0);
+    draw_and_save_minus_background(MppiChi2Wide, MppiChi2bcg, folderWithDiagonal, "MppiFit", "p^{+}#pi^{-}", 1.1);
+    draw_and_save_minus_background(MpipChi2Wide, MpipChi2bcg, folderWithDiagonal, "MpipFit", "#pi^{+}p^{-}", 1.1);
+    draw_and_save_minus_background(MKKChi2Wide, MKKChi2bcg, folderWithDiagonal, "MKKFit", "K^{+}K^{-}", 1.1);
+    draw_and_save_minus_background(MpipiChi2Wide, MpipiChi2bcg, folderWithDiagonal, "MpipiFit", "#pi^{+}#pi^{-}", 0.8);
+    draw_and_save_minus_background(MppChi2Wide, MppChi2bcg, folderWithDiagonal, "MppFit", "p^{+}p^{-}", 2.4);
+    draw_and_save(MKpiChi2bcg, folderWithDiagonal, "MKpibcg", "K^{+}#pi^{-} - background");
+    draw_and_save(MpiKChi2bcg, folderWithDiagonal, "MpiKbcg", "#pi^{+}K^{-} - background");
+    draw_and_save(MppiChi2bcg, folderWithDiagonal, "Mppibcg", "p^{+}#pi^{-} - background");
+    draw_and_save(MpipChi2bcg, folderWithDiagonal, "Mpipbcg", "#pi^{+}p^{-} - background");
+    draw_and_save(MKKChi2bcg, folderWithDiagonal, "MKKbcg", "K^{+}K^{-} - background");
+    draw_and_save(MpipiChi2bcg, folderWithDiagonal, "Mpipibcg", "#pi^{+}#pi^{-} - background");
+    draw_and_save(MppChi2bcg, folderWithDiagonal, "Mppbcg", "p^{+}p^{-} - background");
 
     return 0;
 }
 
-void draw_and_save(TH1D* data, std::string folderWithDiagonal, std::string name){
+void draw_and_save(TH1D* data, std::string folderWithDiagonal, std::string name, std::string title){
     TCanvas* resultCanvas = new TCanvas("resultCanvas", "resultCanvas", 4000, 2400);
     data->SetMinimum(0);
     data->SetMarkerStyle(kFullCircle);
     data->SetMarkerSize(2);
     data->SetMarkerColor(kBlue);
     data->SetLineColor(kBlue+2);
+    data->SetTitle(title.c_str());
     //TODO: add fitting
     data->Draw("hist");
     data->SetLineWidth(2);
@@ -86,7 +117,27 @@ void draw_and_save(TH1D* data, std::string folderWithDiagonal, std::string name)
     data->GetXaxis()->SetTitleSize(0.05);
     data->GetYaxis()->SetLabelSize(0.05);
     data->GetYaxis()->SetTitleSize(0.05);
-    resultCanvas->SetMargin(0.15, 0.05, 0.15, 0.05);
-    gPad->Update();
+    resultCanvas->SetMargin(0.15, 0.05, 0.15, 0.10);
+    data->SetTitle(title.c_str());
+    // gPad->Update();
+    resultCanvas->SaveAs((folderWithDiagonal+name+".pdf").c_str());
+}
+
+void draw_and_save_minus_background(TH1D* data, TH1D* bcg, std::string folderWithDiagonal, std::string name, std::string title, double bcg_region){
+    MyStyles styleLibrary;
+    TStyle tempStyle = styleLibrary.Hist2DQuarterSize(true);
+    tempStyle.cd();
+    gROOT->ForceStyle();
+    TCanvas* resultCanvas = new TCanvas("resultCanvas", "resultCanvas", 4000, 2400);
+    int bcg_bin = data->FindBin(bcg_region);
+    bcg->Scale(data->Integral(bcg_bin, -1)/bcg->Integral(bcg_bin, -1));
+    data->Add(bcg, -1.);
+    data->SetMarkerStyle(kFullCircle);
+    data->SetMarkerSize(2);
+    data->SetMarkerColor(kBlue);
+    data->SetTitle(title.c_str());
+    //TODO: add fitting
+    data->Draw("e");
+
     resultCanvas->SaveAs((folderWithDiagonal+name+".pdf").c_str());
 }
