19c2f36e2b5a6626bff6965cd04af1beb885455b
AddahDraggon <adam.jan@vp.pl>
2025-10-13 20:49:36 -0400

    Cleaning up starsim scripts

--- Inclusive_Analysis/starsim/STAR_scripts/reconstruct_all.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-starver pro
-
-root4star -q -b bfc.C\(0,100,\"DbV20200225,DbV20211220_TPC_Calibrations,StiCA,pp2017a,btof,mtd,mtdCalib,pp2pp,PicoVtxDefault,UseBTOFmatchOnly,VFStoreX,fmsDat,fmsPoint,fpsDat,BEmcChkStat,OSpaceZ2,OGridLeakFull,TpxClu,evout,-hitfilt,mtdsim,MiniMcMk,McAna,emcSim,EEfs,GeantOut,gen_T,geomT,sim_T,TpcRS,-ittf,-tpc_daq,nodefault,fzin,ry2017a\",\"elastic.fzd\"\)
-root4star -q -b bfc.C\(0,100,\"DbV20200225,DbV20211220_TPC_Calibrations,StiCA,pp2017a,btof,mtd,mtdCalib,pp2pp,PicoVtxDefault,UseBTOFmatchOnly,VFStoreX,fmsDat,fmsPoint,fpsDat,BEmcChkStat,OSpaceZ2,OGridLeakFull,TpxClu,evout,-hitfilt,mtdsim,MiniMcMk,McAna,emcSim,EEfs,GeantOut,gen_T,geomT,sim_T,TpcRS,-ittf,-tpc_daq,nodefault,fzin,ry2017a\",\"singleDiffractive.fzd\"\)
-root4star -q -b bfc.C\(0,100,\"DbV20200225,DbV20211220_TPC_Calibrations,StiCA,pp2017a,btof,mtd,mtdCalib,pp2pp,PicoVtxDefault,UseBTOFmatchOnly,VFStoreX,fmsDat,fmsPoint,fpsDat,BEmcChkStat,OSpaceZ2,OGridLeakFull,TpxClu,evout,-hitfilt,mtdsim,MiniMcMk,McAna,emcSim,EEfs,GeantOut,gen_T,geomT,sim_T,TpcRS,-ittf,-tpc_daq,nodefault,fzin,ry2017a\",\"doubleDiffractive.fzd\"\)
-root4star -q -b bfc.C\(0,100,\"DbV20200225,DbV20211220_TPC_Calibrations,StiCA,pp2017a,btof,mtd,mtdCalib,pp2pp,PicoVtxDefault,UseBTOFmatchOnly,VFStoreX,fmsDat,fmsPoint,fpsDat,BEmcChkStat,OSpaceZ2,OGridLeakFull,TpxClu,evout,-hitfilt,mtdsim,MiniMcMk,McAna,emcSim,EEfs,GeantOut,gen_T,geomT,sim_T,TpcRS,-ittf,-tpc_daq,nodefault,fzin,ry2017a\",\"centralDiffractive.fzd\"\)
-root4star -q -b bfc.C\(0,100,\"DbV20200225,DbV20211220_TPC_Calibrations,StiCA,pp2017a,btof,mtd,mtdCalib,pp2pp,PicoVtxDefault,UseBTOFmatchOnly,VFStoreX,fmsDat,fmsPoint,fpsDat,BEmcChkStat,OSpaceZ2,OGridLeakFull,TpxClu,evout,-hitfilt,mtdsim,MiniMcMk,McAna,emcSim,EEfs,GeantOut,gen_T,geomT,sim_T,TpcRS,-ittf,-tpc_daq,nodefault,fzin,ry2017a\",\"nonDiffractive.fzd\"\)
---
+++ Inclusive_Analysis/starsim/starsim_and_bfc/reconstruct_centralDiffractive.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+starver pro
+
+root4star -q -b 'bfc.C(0,100,"DbV20200225,y2017a,agml,StiCA,btof,mtd,mtdCalib,pp2pp,PicoVtxDefault,UseBTOFmatchOnly,VFStoreX,fmsDat,fmsPoint,fpsDat,OSpaceZ2,OGridLeak3D,fzin,usexgeom,l0,FieldOn,MakeEvent,Idst,BAna,Tree,Tree,logger,genvtx,TpcRS,TpxClu,tpcDb,bbcSim,btofsim,btofMixer,btofMatch,emcy2,eefs,geantout,-dstout,IdTruth,big,clearmem,CorrX,gen_T,geomT,sim_T,evout,TpcHitMover,tags,MiniMcMk,sdt20170324.230639","results/centralDiffractive.fzd")'
