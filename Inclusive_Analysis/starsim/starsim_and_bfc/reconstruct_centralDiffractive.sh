f97e76513e3db8abfc50338586ffbfc2eaad6145
AddahDraggon <adam.jan@vp.pl>
2025-12-08 11:06:20 -0500

    Cleaning up for readability

--- Inclusive_Analysis/starsim/starsim_and_bfc/reconstruct_centralDiffractive.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-starver pro
-
-# root4star -q -b 'bfc.C(0,'$1',"DbV20200225,y2017a,agml,StiCA,btof,mtd,mtdCalib,pp2pp,PicoVtxDefault,UseBTOFmatchOnly,VFStoreX,fmsDat,fmsPoint,fpsDat,OSpaceZ2,OGridLeak3D,fzin,usexgeom,l0,FieldOn,MakeEvent,Idst,BAna,Tree,Tree,logger,genvtx,TpcRS,TpxClu,tpcDb,bbcSim,btofsim,btofMixer,btofMatch,emcy2,eefs,geantout,-dstout,IdTruth,big,clearmem,CorrX,gen_T,geomT,sim_T,evout,TpcHitMover,tags,MiniMcMk,sdt20170324.230639","results/centralDiffractive.fzd")'
-
-root4star -q -b 'bfc.C(0,5,"DbV20200225,y2017a,agml,StiCA,btof,mtd,mtdCalib,pp2pp,PicoVtxDefault,UseBTOFmatchOnly,VFStoreX,fmsDat,fmsPoint,fpsDat,OSpaceZ2,OGridLeak3D,fzin,usexgeom,l0,FieldOn,MakeEvent,Idst,BAna,Tree,Tree,logger,genvtx,TpcRS,TpxClu,tpcDb,bbcSim,btofsim,btofMixer,btofMatch,emcy2,eefs,geantout,-dstout,IdTruth,big,clearmem,CorrX,gen_T,geomT,sim_T,evout,TpcHitMover,tags,MiniMcMk,sdt20170324.230639,emcDY2,BtofDat,picoWrite","results/centralDiffractive.fzd")'
--- Inclusive_Analysis/starsim/starsim_and_bfc/run_all.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-starver pro
-
-root4star -q -b elastic.C\($1\)
-root4star -q -b singleDiffractive.C\($1\)
-root4star -q -b doubleDiffractive.C\($1\)
-root4star -q -b centralDiffractive.C\($1\)
-root4star -q -b nonDiffractive.C\($1\)
