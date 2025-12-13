4008a049320bea186a163718031d7befabbe6c4f
AddahDraggon <adam.jan@vp.pl>
2025-12-13 18:25:47 -0500

    Fixed generator for future use in special cases

--- Inclusive_Analysis/starsim/starsim_and_bfc/centralDiffractive_generator_start.sh
+++ Inclusive_Analysis/starsim/starsim_and_bfc/centralDiffractive_generator_start.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+#creating output directory
+output_directory=/star/data05/scratch/adamwatroba/generator_output
+mkdir -p $output_directory
+
 # setting default values
 length=$1
 length="${length:-0}"
@@ -8,10 +12,25 @@ run_number="${run_number:-0}"
 seed=$3
 seed="${seed:-0}"
 
+#show help if there is 0 arguments provided
+if [ "$#" -eq "0" ]; then
+    echo Usage:
+    echo ./centralDiffractive_generator_start.sh [number of events] [run number \(optional, default 18091010\)] ["seed" \(optional, default 0\)]
+fi
+
 # finishing if there is none (0) events
 if [ "$length" -eq "0" ]; then
     exit 0
 fi
+
+#creating file with info about simulation
+newfile=$output_directory/generator_data.txt
+echo Number of events: > $newfile
+echo $length >> $newfile
+echo Run number \(if 0 then actual one is 18091010\): >> $newfile
+echo $run_number >> $newfile
+echo Seed: >> $newfile
+echo $seed >> $newfile
 
 # modulo to cut it into pieces of n events
 n=100
