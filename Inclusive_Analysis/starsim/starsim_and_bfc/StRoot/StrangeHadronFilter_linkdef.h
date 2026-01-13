c5607366e1151a948850487b9ceb23095b37670e
AddahDraggon <adam.jan@vp.pl>
2026-01-13 11:32:54 -0500

    Recompiled the library after adding missing classes to linkdef

--- Inclusive_Analysis/starsim/starsim_and_bfc/StRoot/StrangeHadronFilter_linkdef.h
+++ Inclusive_Analysis/starsim/starsim_and_bfc/StRoot/StrangeHadronFilter_linkdef.h
@@ -5,6 +5,11 @@
 #pragma link off all functions;
 
 #pragma link C++ class StrangeHadronFilter+;
+#pragma link C++ class ParticleK0SFilter+;
+#pragma link C++ class ParticleLambda0Filter+;
+#pragma link C++ class ParticleKstarFilter+;
+#pragma link C++ class ParticlePhiFilter+;
+
 #pragma link C++ class StarFilterMaker+;
 #pragma link C++ class StMaker-;
 
