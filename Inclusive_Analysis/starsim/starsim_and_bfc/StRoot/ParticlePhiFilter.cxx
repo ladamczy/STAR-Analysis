62f1f803aed6bf3fd3ac32397f2b63e24bf8c63c
AddahDraggon <adam.jan@vp.pl>
2026-01-17 16:42:20 -0500

    Fixed production of short-lived particles. Those do not get to the Geant - Pythia decays those

--- Inclusive_Analysis/starsim/starsim_and_bfc/StRoot/ParticlePhiFilter.cxx
+++ Inclusive_Analysis/starsim/starsim_and_bfc/StRoot/ParticlePhiFilter.cxx
@@ -20,28 +20,20 @@ Int_t ParticlePhiFilter::Filter(StarGenEvent* event){
     //loop through all particles
     //if there is no phi, event gets rejected
     //if there is even one - it gets accepted
-    //also, there needs to be at least one particle with mStack>0
-    bool hasStrangeParticle = false;
-    bool hasParticleWithPositiveStack = false;
+    //due to short decay time, the decay is handled by pythia, and therefore it does not come out to the Geant
+    //so checks like:
+
+    //particle->GetStatus()!=1
+    //particle->GetStack()>0
+
+    //have been removed
     StarGenParticle* particle = nullptr;
     for(size_t i = 0; i<event->GetNumberOfParticles(); i++){
         particle = (*event)[i];
-        if(particle->GetStatus()!=1){
-            continue;
-        }
         //strange particle check
         if(abs(particle->GetId())==333){
             //if there is a particle like that
             //event can be accepted
-            hasStrangeParticle = true;
-        }
-        //positive stack check
-        if(particle->GetStack()>0){
-            hasParticleWithPositiveStack = true;
-        }
-
-        //decision
-        if(hasStrangeParticle&&hasParticleWithPositiveStack){
             return StarGenEvent::kAccept;
         }
     }
