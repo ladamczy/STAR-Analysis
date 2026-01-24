b2d781c0e09e9f376cfcec4f04970bd902936fba
AddahDraggon <adam.jan@vp.pl>
2026-01-24 07:23:08 -0500

    Compilation change from make to cons (native to STAR)

--- Inclusive_Analysis/starsim/starsim_and_bfc/StRoot/compile.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-starver pro
-
-rootcint -f StrangeHadronFilter_dict.cxx -c -I$STAR/StRoot -I$STAR/StRoot/StChain -I$STAR/StRoot/St_base -I$STAR/StRoot/Star2Root *Filter.h StarGenerator/FILT/StarFilterMaker.h StMaker.h StrangeHadronFilter_linkdef.h
-
-gmake -f Makefile
---
+++ Inclusive_Analysis/starsim/starsim_and_bfc/compile.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+starver pro
+
+cons
+
+rm temp_gccflags.c
