10872f6e8e64e16a9e514febf0cd15253dd81a49
AddahDraggon <adam.jan@vp.pl>
2026-03-01 12:06:44 -0500

    Fixed the phi(1020) filter to reject decays far away from detection range

--- Inclusive_Analysis/starsim/starsim_and_bfc/StRoot/StarGenerator/FILT/ParticlePhiFilter.cxx
+++ Inclusive_Analysis/starsim/starsim_and_bfc/StRoot/StarGenerator/FILT/ParticlePhiFilter.cxx
@@ -26,14 +26,36 @@ Int_t ParticlePhiFilter::Filter(StarGenEvent* event){
     //particle->GetStatus()!=1
     //particle->GetStack()>0
 
-    //have been removed
+    //have been removed and replaced
     StarGenParticle* particle = nullptr;
     for(size_t i = 0; i<event->GetNumberOfParticles(); i++){
         particle = (*event)[i];
         //strange particle check
-        if(abs(particle->GetId())==333){
-            //if there is a particle like that
-            //event can be accepted
+        if(abs(particle->GetId())!=333){
+            continue;
+        }
+        if(particle->GetStatus()!=2){
+            continue;
+        }
+        //checking if the children fall into (roughly) fiducial region for the detection
+        //here |eta|<0.9+0.1, pT>0.2-0.05
+        double eta = 0.9;
+        double d_eta = 0.3;
+        double pT = 0.2;
+        double d_pT = 0.05;
+        TLorentzVector firstDaughter = (*event)[particle->GetFirstDaughter()]->momentum();
+        TLorentzVector lastDaughter = (*event)[particle->GetLastDaughter()]->momentum();
+        if(fabs(firstDaughter.Eta())>=eta+d_eta||firstDaughter.Pt()<pT-d_pT){
+            continue;
+        }
+        if(fabs(lastDaughter.Eta())>=(eta+d_eta)||lastDaughter.Pt()<(pT-d_pT)){
+            continue;
+        }
+        //checking the children (all combinations)
+        if((*event)[particle->GetFirstDaughter()]->GetId()==321&&(*event)[particle->GetLastDaughter()]->GetId()==-321){
+            return StarGenEvent::kAccept;
+        }
+        if((*event)[particle->GetFirstDaughter()]->GetId()==-321&&(*event)[particle->GetLastDaughter()]->GetId()==321){
             return StarGenEvent::kAccept;
         }
     }
