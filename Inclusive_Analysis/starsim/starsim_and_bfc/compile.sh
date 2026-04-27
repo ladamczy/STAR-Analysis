55fa630f1c446d2538572029e57601768e467214
AddahDraggon <adam.jan@vp.pl>
2026-04-27 16:20:33 -0400

    Changing STAR scheduling and compiling because of ALMA9 transfer

--- Inclusive_Analysis/starsim/starsim_and_bfc/compile.sh
+++ Inclusive_Analysis/starsim/starsim_and_bfc/compile.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+singularity shell --shell /usr/bin/csh -B /direct -B /star -B /afs -B /gpfs -B /sdcc/lustre02 /cvmfs/star.sdcc.bnl.gov/containers/rhic_sl7.sif <<EOF
+
 starver pro
-
 cons
-
 rm temp_gccflags.c
+EOF
