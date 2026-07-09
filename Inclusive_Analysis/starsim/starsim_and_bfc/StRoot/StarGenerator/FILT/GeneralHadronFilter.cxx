1947c8545026140d8d206f916e5d87f30302eae1
AddahDraggon <adam.jan@vp.pl>
2026-07-09 16:10:01 -0400

    Added RP fiducial region (used once, so far not very useful)

--- Inclusive_Analysis/starsim/starsim_and_bfc/StRoot/StarGenerator/FILT/GeneralHadronFilter.cxx
+++ Inclusive_Analysis/starsim/starsim_and_bfc/StRoot/StarGenerator/FILT/GeneralHadronFilter.cxx
@@ -26,6 +26,25 @@ Int_t GeneralHadronFilter::Filter(StarGenEvent* event){
     for(size_t i = 0; i<event->GetNumberOfParticles(); i++){
         particle = (*event)[i];
 
+        //scattered proton fiducial region special thing (currently off)
+        //ID 3 and 4 because (excerpt from PYTHIA production):
+        // [   0|   0|  -1] id=         0    Rootino stat=-201 p=(   0.000,   0.000,   0.000,   0.000;  510.000) v=(  0.0000,  0.0000,   0.000) [2212 2212] [0 15]
+        // [   1|   1|  -1] id=      2212     proton stat=04 p=(  -0.241,   0.112, 254.998, 255.000;    0.938) v=( -0.1651, -0.1171,  46.621) [0 0] [3 5]
+        // [   2|   2|  -1] id=      2212     proton stat=04 p=(   0.241,  -0.112,-254.998, 255.000;    0.938) v=( -0.1651, -0.1171,  46.621) [0 0] [3 5]
+        // [   3|   3|  -1] id=      2212     proton stat=01 p=(  -0.360,   0.174, 254.969, 254.971;    0.938) v=( -0.1651, -0.1171,  46.621) [1 2] [0 0]
+        // [   4|   4|  -1] id=      2212     proton stat=01 p=(  -0.151,  -0.201,-213.968, 213.970;    0.938) v=( -0.1651, -0.1171,  46.621) [1 2] [0 0]
+        // [   5|   5|  -1] id=   9900110            stat=15 p=(   0.511,   0.028, -41.001,  41.058;  
+        // if(i==3||i==4){
+        //     double px = particle->GetPx();
+        //     double py = particle->GetPy();
+        //     bool f1 = (0.4<abs(py)&&abs(py)<0.8);
+        //     bool f2 = (-0.27<px);
+        //     bool f3 = (pow(px+0.6, 2)+pow(py, 2)<1.25);
+        //     if(!(f1&&f2&&f3)){
+        //         return StarGenEvent::kReject;
+        //     }
+        // }
+
         //"stable" particle check
         switch(abs(particle->GetId())){
         case 111:// pi0 stable to permit mother/daughter in star record
@@ -66,7 +85,7 @@ Int_t GeneralHadronFilter::Filter(StarGenEvent* event){
         //decision
         if(hasAlreadyOneParticleInTPCFiducialRegion){
             return StarGenEvent::kAccept;
-        }else{
+        } else{
             hasAlreadyOneParticleInTPCFiducialRegion = true;
         }
     }
