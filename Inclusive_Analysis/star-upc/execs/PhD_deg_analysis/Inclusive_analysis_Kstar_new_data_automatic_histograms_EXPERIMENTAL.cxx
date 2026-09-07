bdc4b9132866c67fff3a51fd4fa2b5d0d1f680e9
Adam W <adam.jan@vp.pl>
2026-09-08 00:33:28 +0200

    Added mistakenly removed initial background size

--- Inclusive_Analysis/star-upc/execs/PhD_deg_analysis/Inclusive_analysis_Kstar_new_data_automatic_histograms_EXPERIMENTAL.cxx
+++ Inclusive_Analysis/star-upc/execs/PhD_deg_analysis/Inclusive_analysis_Kstar_new_data_automatic_histograms_EXPERIMENTAL.cxx
@@ -173,6 +173,7 @@ int main(int argc, char* argv[]){
 
                 //setting initial fitting values
                 double initial_bcg_scale = sig_slice->GetBinContent(sig_slice->FindBin(fitMaximum+fitWidth))/bcg_slice->GetBinContent(bcg_slice->FindBin(fitMaximum+fitWidth));
+                fit_func_bcg.SetParameters(initial_bcg_scale, 0., 0.);
                 fit_func_bcg.SetParLimits(0, 0., 10.);
                 double initial_sig_height = (sig_slice->GetBinContent(sig_slice->FindBin(fitMaximum))-sig_slice->GetBinContent(sig_slice->FindBin(fitMaximum+fitWidth)))*TMath::PiOver2()*fitWidth;
                 fit_func_sig.SetParameters(initial_sig_height, fitMaximum, fitWidth);
